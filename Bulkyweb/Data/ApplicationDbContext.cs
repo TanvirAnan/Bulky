90d57d0b538e9c7d7382175d92be1c295a48699d
TanvirAnan <tanvirhassanananta23@gmail.com>
2024-06-25 18:04:19 +0600

    First Migration done and Catogories table created

---
+++ Bulkyweb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Bulkyweb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulkyweb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+                    
+        }
+        public DbSet<Catagory> Catagories { get; set; }
+    }
+}
